1ae109425f3e8d91956a15c22dc849ac698c17e5
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2012-12-05 16:08:05 +0000

    Added multi-missing, extra and multi-extra tests

--- run-tests.sh
+++ run-tests.sh
@@ -5,8 +5,22 @@ echo "Matching paras test"
 diff -s tests/matching/expected-output tests/matching/output
 echo
 
-
 echo "Missing para test"
 ./match_paras.py tests/missing/input tests/missing/template tests/missing/output
 diff -s tests/missing/expected-output tests/missing/output
+echo
+
+echo "Multiple missing para test"
+./match_paras.py tests/multi-missing/input tests/multi-missing/template tests/multi-missing/output
+diff -s tests/multi-missing/expected-output tests/multi-missing/output
+echo
+
+echo "Extra para test"
+./match_paras.py tests/extra/input tests/extra/template tests/extra/output
+diff -s tests/extra/expected-output tests/extra/output
+echo
+
+echo "Multiple extra para test"
+./match_paras.py tests/multi-extra/input tests/multi-extra/template tests/multi-extra/output
+diff -s tests/multi-extra/expected-output tests/multi-extra/output
 echo
