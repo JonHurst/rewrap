463be17edbdbb9d537e2a859c2146600d5196da0
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2012-12-05 14:44:05 +0000

    Allow dumptokens to dump to a file

--- common.py
+++ common.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
 import tokenise
 
-def dump_tokens(token_list, astext=False):
+def dump_tokens(token_list, astext=False, outfh=sys.stdout):
+    outstr = ""
     if astext:
-        outstr = ""
         for t in token_list:
             outstr += t[0]
-        print outstr
     else:
         for t in token_list:
-            print "{", tokenise.token_description[t[1]], "}:", t[0]
-
+            outstr += "{%s}: %s\n" % (tokenise.token_description[t[1]], t[0])
+    outfh.write(outstr.encode("utf-8") + "\n")
 
