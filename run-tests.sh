b2c7431754adce930c2243bd2f127bb1bbc11531
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2012-12-05 15:13:03 +0000

    Add missing paragraph test

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-./match_paras.py tests/matching/input tests/matching/template tests/matching/output
 echo "Matching paras test"
+./match_paras.py tests/matching/input tests/matching/template tests/matching/output
 diff -s tests/matching/expected-output tests/matching/output
 echo
 
+
+echo "Missing para test"
+./match_paras.py tests/missing/input tests/missing/template tests/missing/output
+diff -s tests/missing/expected-output tests/missing/output
+echo
