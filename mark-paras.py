11bd5eccb18d3b3273265ffd05f9a23317e49979
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2012-12-20 13:43:15 +0000

    Updated mark-paras to use the same numbering scheme as match_paras

--- mark-paras.py
+++ mark-paras.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python
 
 import sys
+import re
 
 text = unicode(sys.stdin.read(), "utf-8")
-c = 0
-if text[0] == u"\n":
-    sys.stdout.write(u"\\\\0000\\\\\n")
-    c = 1
-    text = text[1:]
-paras = text.split(u"\n\n")
-for p in paras:
+paras = re.split(r"\n\n+", text)
+for c, p in enumerate(paras):
     p = (ur"\\%04d\\" % c) + p + u"\n\n"
     sys.stdout.write(p.encode("utf-8"))
-    c += 1
