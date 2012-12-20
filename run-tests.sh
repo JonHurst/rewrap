e933c43969aa261835f1329e3e349478ce51d5b2
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2012-12-20 10:34:34 +0000

    Updated split, split-and-join and pg84-1 tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for X in matching missing multi-missing extra multi-extra moved changed join multi-join
+TESTS="matching missing multi-missing extra multi-extra \
+moved changed join multi-join split split-and-join pg84-1"
+
+if [ $# -ge 1 ]
+then
+    TESTS=$*
+fi
+
+for X in $TESTS
 do
     echo "------------------------------"
     echo "${X} test"
@@ -15,18 +23,3 @@ done
 exit
 
 #not yet working: multi-moved
-
-echo "Join multiple paras test"
-./match_paras.py tests/multi-join/input tests/multi-join/template tests/multi-join/output
-diff -s tests/multi-join/expected-output tests/multi-join/output
-echo
-
-echo "Split paras test"
-./match_paras.py tests/split/input tests/split/template tests/split/output
-diff -s tests/split/expected-output tests/split/output
-echo
-
-echo "Splits and joins test"
-./match_paras.py tests/split-and-join/input tests/split-and-join/template tests/split-and-join/output
-diff -s tests/split-and-join/expected-output tests/split-and-join/output
-echo
